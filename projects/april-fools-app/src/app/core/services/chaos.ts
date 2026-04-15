5e8b62704765c8ee7cccb9ffc83ef77f0fce809f
Bongane Lebopo <blebopo@dvtsoftware.com>
2026-04-15 05:17:11 +0200

    Added chaos to the sliders.

---
+++ projects/april-fools-app/src/app/core/services/chaos.ts
@@ -0,0 +1,20 @@
+import { Injectable, Signal, WritableSignal } from "@angular/core";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ChaosService{
+  intervalId!: ReturnType<typeof setInterval>;
+
+  startChaos(realValue: Signal<number>, fakeValue: WritableSignal<number>){
+    this.intervalId = setInterval(() => {
+      const lie = realValue() + Math.floor(Math.random() * 20) - 10;
+      fakeValue.set(lie);
+      setTimeout(() => fakeValue.set(realValue()), 300);
+    }, 2000); // Every two seconds figit with displayed value
+  }
+
+  stop(): void {
+    clearInterval(this.intervalId);
+  }
+}
--- projects/april-fools-app/src/app/features/step-two/customize.ts
+++ projects/april-fools-app/src/app/features/step-two/customize.ts
@@ -1,4 +1,4 @@
-import { Component, input, model, output } from '@angular/core';
+import { Component, inject, input, model, OnDestroy, output, signal } from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatCard, MatCardModule } from '@angular/material/card';
 import { MatDivider } from '@angular/material/divider';
@@ -9,6 +9,7 @@ import { MatIcon } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButton, MatMiniFabButton, MatIconButton } from '@angular/material/button';
 import { SyrupDetails } from '../../model/syrup';
+import { ChaosService } from '../../core/services/chaos';
 
 @Component({
   selector: 'app-customize',
@@ -27,7 +28,7 @@ import { SyrupDetails } from '../../model/syrup';
   templateUrl: './customize.html',
   styleUrl: './customize.css',
 })
-export class Customize {
+export class Customize implements OnDestroy{
   availableSyrups = model<Syrup[]>([]);
   selectedSyrups = model<SyrupDetails[]>([]);
  
@@ -36,7 +37,17 @@ export class Customize {
   review = output<void>();
 
   temperature = model.required<number>();
+  displayTemp = signal(70); // fake temperature
+
   foamDensity = model.required<number>();
+  displayFoam = signal(7); // fake foam
+
+  private chaosService = inject(ChaosService);
+
+  constructor() {
+    this.chaosService.startChaos(this.temperature, this.displayTemp);
+    this.chaosService.startChaos(this.foamDensity, this.displayFoam);
+  }
 
   onTemperatureInput(event: Event) {
     this.temperature.set((event.target as HTMLInputElement).valueAsNumber);
@@ -91,5 +102,9 @@ export class Customize {
   onReview(event: MouseEvent) {
     (event.currentTarget as HTMLButtonElement).blur(); // Remove focus from cuurrent tab, for keyboard navigation and screen readers
     this.review.emit();
+  }
+
+  ngOnDestroy(): void {
+    this.chaosService.stop();
   }
 }
